900aa6c07a5a0dfd5522e33689c42822c8b38c14
sam64mc <sam64mc@gmail.com>
2017-09-29 12:44:30 +0200

    Update Average.java
    mofif methode average

--- Average.java
+++ Average.java
@@ -3,12 +3,12 @@ class Average {
 	public static float average(int[] values) {
 		float total = 0;
 
-		// début de ton code ici !
+		for(int i=0; i < average.length ; i++)
+		total = total + average[i];
 
+		float moyenne = total / average.length;
+ 
 
-
-		// fin de ton code :)
-
-		return total / values.length;
+		return total / average.length;
 	}
 }
